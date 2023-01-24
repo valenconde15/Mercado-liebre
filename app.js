22cffec930d3da21a8c6d975e6cac47478f29ae2
valenconde15 <valeconde-2004@hotmail.com>
2023-01-24 15:15:29 -0300

    "final"

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ const path = require("path")
 const PORT =process.env.PORT || 3000
 
 app.use(express.static("public")) 
+app.use(express.static("views"))
+
 
 app.get("/", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, ("./views/home.html")))
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+
+app.get("/", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+})
+
+app.get("/", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
 })
 
 app.listen(PORT, () => {
